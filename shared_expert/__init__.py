1431b48692f2452e0ec9293796a9e4eda5dc192b
maga-484 <magaligafe@gmail.com>
2026-05-13 20:03:15 -0300

    chore: remove unused csiro shared_expert folder

--- shared_expert/__init__.py
+++
@@ -1,2 +0,0 @@
-﻿# shared_expert/__init__.py
-
--- shared_expert/utils.py
+++
@@ -1,9 +0,0 @@
-﻿import psutil
-import torch
-
-def get_memory_usage():
-    ram = psutil.Process().memory_info().rss / 1024**2
-    vram = 0.0
-    if torch.cuda.is_available():
-        vram = torch.cuda.memory_allocated() / 1024**2
-    return ram, vram
